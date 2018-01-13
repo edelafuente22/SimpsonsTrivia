6e9f747af729984167a71d3f773bd650b5c93833
edelafuente22 <ofthefountain22@gmail.com>
2018-01-12 17:59:13 -0800

    Initial Commit

---
+++ Simpsons Trivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simpsons Trivia
+//
+//  Created by Edward de la Fuente on 1/12/18.
+//  Copyright © 2018 Edward de la Fuente. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
